46d19d3ae99e80da011644307e969f38214ae109
mauryav <123108500+mauryav@users.noreply.github.com>
2023-01-28 12:29:35 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -61,23 +61,23 @@ public class CardTrick {
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
-        System.out.println();
+        System.out.println("You Won");
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
-        System.out.println();
+        System.out.println("My name is Vikas Maurya, but you can call me Vikas");
+        System.out.println("I am persuaing diploma in computer programming at Sheridan College");
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Start a business");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
+	System.out.println("-- Make my parents proud");	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- Being Social");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Gym");
 
-        System.out.println();
+        System.out.println("Thank you for playing");
         
     
     }
