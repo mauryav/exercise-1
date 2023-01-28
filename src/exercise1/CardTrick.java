9f64cae74033e7e633fe16ae5e43794bb2f04f5e
mauryav <mauryav@sheridancollege.ca>
2023-01-28 12:46:13 -0500

    game v1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -56,6 +56,8 @@ public class CardTrick {
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
      * @author Paul Bonenfant Jan 2022
+     * @author Vikas Maurya  Jan 28, 2022
+     * I am done!
      */
  
     private static void printInfo() {
